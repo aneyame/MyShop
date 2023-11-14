e5664224eded6eaf4eb380c3262b9452371bfbbe
PtitPotam <steven.guillermet@epitech.digital>
2023-11-14 10:18:27 +0100

    route admin,

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import products from '../views/Products.vue'
+import admin from '../views/admin.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,12 @@ const router = createRouter({
       path: '/products',
       name: 'products',
       component: products
+    },
+    
+    {
+      path: '/admin',
+      name: 'admin',
+      component: admin
     },
   ]
 })
