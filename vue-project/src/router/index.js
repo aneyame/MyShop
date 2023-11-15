280b97e9deb359680282c60f703d9f3e1949196a
aneyame <146078255+aneyame@users.noreply.github.com>
2023-11-15 18:02:35 +0100

    login back

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -3,6 +3,7 @@ import Home from '../views/Home.vue'
 import products from '../views/Products.vue'
 import admin from '../views/admin.vue'
 import login from '../views/login.vue'
+// import { createStore } from 'vuex';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -29,7 +30,18 @@ const router = createRouter({
     },
 
 
-  ]
+  ],
+  state: {
+    token: null,
+  },
+  mutations: {
+    setToken(state, token) {
+      state.token = token;
+    },
+  },
+  getters: {
+    getToken: (state) => state.token,
+  },
 })
 
 export default router
