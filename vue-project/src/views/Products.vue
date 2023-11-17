21e7dd6a9a32c286d96ae5b803f0e6e6376583ed
aneyame <146078255+aneyame@users.noreply.github.com>
2023-11-17 14:50:45 +0100

    Update Products.vue

--- vue-project/src/views/Products.vue
+++ vue-project/src/views/Products.vue
@@ -1,12 +1,17 @@
 <template> 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
   <div class="w-[1512px] h-[982px] pl-24 pr-[140px] pt-[106px] bg-neutral-900 justify-start items-center inline-flex">
     <div class="self-stretch flex-col justify-start items-start gap-2.5 inline-flex">
+=======
+    <div class="w-[1512px] h-[982px] pl-24 pr-[140px] pt-[106px] bg-neutral-900 justify-start items-center inline-flex">
+    <div class="w-[1276px] self-stretch flex-col justify-start items-start gap-2.5 inline-flex">
+>>>>>>> Stashed changes
         <div class="w-[1276px] h-[885px] relative">
             <div class="w-[464px] h-[549px] px-5 py-[25px] left-[812px] top-0 absolute bg-neutral-900 rounded-[10px] border border-neutral-400 flex-col justify-start items-start gap-[22px] inline-flex">
-                <div class="self-stretch h-[100px] flex-col justify-start items-start gap-3 flex">
-                    <div class="self-stretch text-white text-4xl font-bold font-['Arial']">Product Name</div>
+                <div class="h-[100px] flex-col justify-start items-start gap-3 flex">
+                    <div class="self-stretch text-white text-4xl font-bold font-['Arial']">Product name</div>
                     <div class="self-stretch text-zinc-400 text-base font-normal font-['Arial']">Category</div>
                     <article>
                         <span class="fa fa-star checked"></span>
@@ -17,14 +22,14 @@
                     </article>
                 </div>
                 <div class="w-[424px] text-right text-emerald-500 text-4xl font-normal font-['Arial']">99€</div>
-                <div class="self-stretch h-[42px] flex-col justify-start items-start gap-1.5 flex">
+                <div class="h-[42px] flex-col justify-start items-start gap-1.5 flex">
                     <div class="self-stretch text-white text-base font-normal font-['Arial']">Delivery</div>
                     <div class="self-stretch text-zinc-400 text-sm font-normal font-['Arial']">Fast shipping available</div>
                 </div>
-                <div class="self-stretch py-[18px] bg-blue-600 rounded-[10px] justify-center items-center gap-2.5 inline-flex">
+                <div class="w-[424px] py-[18px] bg-blue-600 rounded-[10px] justify-center items-center gap-2.5 inline-flex">
                     <div class="grow shrink basis-0 text-center text-white text-xl font-semibold font-['Arial']">Add to cart</div>
                 </div>
-                <div class="self-stretch h-[165px] pb-2.5 flex-col justify-start items-start gap-3 flex">
+                <div class="h-[165px] pb-2.5 flex-col justify-start items-start gap-3 flex">
                     <div class="self-stretch text-white text-xl font-normal font-['Arial']">Description</div>
                     <div class="self-stretch text-zinc-400 text-sm font-normal font-['Arial']">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</div>
                 </div>
